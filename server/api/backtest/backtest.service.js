c32df5a9c76b6812d20f1ea250d796f66c3b5993
DESKTOP-4SRJMOF\dvxch <dvxchan@gmail.com>
2018-02-20 02:57:23 -0500

    Fixed function name

--- server/api/backtest/backtest.service.js
+++ server/api/backtest/backtest.service.js
@@ -49,48 +49,7 @@ class BacktestService {
       });
   }
 
-  runTest1(ticker, currentDate, startDate) {
-    let shortTerm = config.shortTerm;
-    let longTerm = config.longTerm;
-    let snapshots = [];
-    return this.getData(ticker, currentDate, startDate)
-      .then(quotes => {
-        for (let i = shortTerm[0]; i < shortTerm[1]; i++) {
-          for (let j = longTerm[0]; j < longTerm[1]; j++) {
-            if (i < j) {
-              console.log("short:", i, " long:", j);
-              let MAs = ReversionService.executeMeanReversion(ReversionService.calcMA, quotes, i, j);
-              let yesterdayDecision = MAs[MAs.length - 1];
-              let recommendedDifference = DecisionService.findDeviation(MAs, startDate);
-
-              let averagesRange = { shortTerm: i, longTerm: j };
-              let returns = DecisionService.calcReturns(MAs, recommendedDifference, startDate);
-              console.log("returns: ", returns.totalReturns);
-
-              snapshots.push({ ...averagesRange, ...returns, recommendedDifference });
-            }
-          }
-        }
-        console.log('Calculations done: ', ticker, new Date());
-        endTime = moment();
-
-        const duration = moment.duration(endTime.diff(startTime)).humanize();
-
-        console.log("Duration: ", duration);
-
-        const fields = ['shortTerm', 'longTerm', 'totalReturns', 'totalTrades', 'recommendedDifference'];
-
-        const csv = json2csv({ data: snapshots, fields: fields });
-
-        fs.writeFile(`${ticker}_analysis_${currentDate}-${startDate}.csv`, csv, function (err) {
-          if (err) throw err;
-          console.log('file saved');
-        });
-        return snapshots;
-      });
-  }
-
-  runTest2(ticker, currentDate, startDate) {
+  runTest(ticker, currentDate, startDate) {
     let shortTerm = config.shortTerm;
     let longTerm = config.longTerm;
     let snapshots = [];
