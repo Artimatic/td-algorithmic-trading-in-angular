15de0abc9673e0cebfe535dcdf4228e38a86d7c7
David Chan <dvxchan@gmail.com>
2019-02-06 00:44:39 -0500

    Removed unused function

--- src/app/shared/services/excel-service.service.ts
+++ src/app/shared/services/excel-service.service.ts
@@ -11,11 +11,6 @@ export class ExcelService {
   constructor() { }
 
   public exportAsExcelFile(json: any[], excelFileName: string): void {
-    // const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    // const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-    // const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
-    // this.saveAsExcelFile(excelBuffer, excelFileName);
-
     /* generate worksheet */
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(json);
 
@@ -26,12 +21,4 @@ export class ExcelService {
     /* save to file */
     XLSX.writeFile(wb, excelFileName + '.xlsx');
   }
-
-  private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], {
-      type: EXCEL_TYPE
-    });
-    FileSaver.saveAs(data, fileName + '_export' + EXCEL_EXTENSION);
-  }
-
 }
