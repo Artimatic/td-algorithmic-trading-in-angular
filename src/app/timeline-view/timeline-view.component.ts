02d98fbdc3eb76688bc6d532aeac85d805d10311
David Chan <dvxchan@gmail.com>
2019-04-13 22:18:34 -0400

    Updated timeline page

--- src/app/timeline-view/timeline-view.component.ts
+++ src/app/timeline-view/timeline-view.component.ts
@@ -13,7 +13,8 @@ import { FormGroup, FormBuilder } from '../../../node_modules/@angular/forms';
 export class TimelineViewComponent implements OnInit {
   public startDate: Date;
   public endDate: Date;
-  chart: Chart;
+  openPriceChart: Chart;
+  closePriceChart: Chart;
   form: FormGroup;
   symbol = 'SPY';
 
@@ -36,54 +37,63 @@ export class TimelineViewComponent implements OnInit {
     const startDate = moment(this.startDate).format('YYYY-MM-DD');
 
     this.backtestService.getTimeline(this.form.value.query, startDate, endDate)
-    .map(result => {
-      const greenSeries = [],
-            redSeries = [],
-            constant = [],
-            counts = [];
+      .map(result => {
 
-      result[0].forEach((day, idx) => {
-        constant.push(day.input[1]);
-        counts.push({});
+        const openPriceSeries = this.parseTimelineData(result, 2);
+        const closePriceSeries = this.parseTimelineData(result, 1);
+
+        this.openPriceChart = this.initChart(openPriceSeries);
+        this.closePriceChart = this.initChart(closePriceSeries);
+
+        return;
+      })
+      .subscribe((timelineData: any) => {
+        console.log('time: ', timelineData);
+      }, error => {
+        console.log('error: ', error);
+        this.snackBar.open(`Error`, 'Dismiss');
       });
+  }
 
-      const constantLen = constant.length;
+  parseTimelineData(timelineMatchArr, referenceIdx) {
+    const greenSeries = [],
+      redSeries = [],
+      constant = [],
+      counts = [];
 
-      if (result.length > 1) {
-        for (let i = 1, c = result.length; i < c; i++) {
-          let hasIntegrity = false;
-          const current = result[i];
-          for (let j = 0, cc = current.length; j < cc; j++) {
-            const signal = current[j].input[1];
-            if (j < constant.length) {
-              if (constant[j] === signal) {
-                hasIntegrity = true;
+    timelineMatchArr[0].forEach((day) => {
+      constant.push(day.input[referenceIdx]);
+      counts.push({});
+    });
+
+    const constantLen = constant.length;
+
+    if (timelineMatchArr.length > 1) {
+      for (let i = 1, c = timelineMatchArr.length; i < c; i++) {
+        const current = timelineMatchArr[i];
+        for (let j = 0, cc = current.length; j < cc; j++) {
+          const signal = current[j].input[referenceIdx];
+          if (j < constant.length) {
+            if (signal) {
+              if (j >= counts.length) {
+                counts.push({ up: 1, down: 0, total: 1 });
               } else {
-                hasIntegrity = false;
-                throw new TypeError('Data incomplete');
+                counts[j].up++;
+                counts[j].total++;
               }
-            } else if (hasIntegrity) {
-              if (signal) {
-                if (j >= counts.length) {
-                  counts.push({up: 1, down: 0, total: 1});
-                } else {
-                  counts[j].up++;
-                  counts[j].total++;
-                }
+            } else {
+              if (j >= counts.length) {
+                counts.push({ up: 0, down: 1, total: 1 });
               } else {
-                if (j >= counts.length) {
-                  counts.push({up: 0, down: 1, total: 1});
-                } else {
-                  counts[j].down++;
-                  counts[j].total++;
-                }
+                counts[j].down++;
+                counts[j].total++;
               }
             }
           }
         }
       }
 
-      greenSeries.push({ x: constantLen - 1, y: constant[constantLen - 1]});
+      greenSeries.push({ x: constantLen - 1, y: constant[constantLen - 1] });
       redSeries.push({ x: constantLen - 1, y: constant[constantLen - 1] });
 
       const countsLen = counts.length;
@@ -97,7 +107,7 @@ export class TimelineViewComponent implements OnInit {
       }
 
 
-      const series = [{
+      return [{
         name: 'Constant',
         data: constant
       }, {
@@ -109,17 +119,7 @@ export class TimelineViewComponent implements OnInit {
         data: redSeries,
         dashStyle: 'dash'
       }];
-
-      this.chart = this.initChart(series);
-
-      return result;
-    })
-      .subscribe((timelineData: any) => {
-        console.log('time: ', timelineData);
-      }, error => {
-        console.log('error: ', error);
-        this.snackBar.open(`Error`, 'Dismiss');
-      });
+    }
   }
 
   initChart(series: any): Chart {
