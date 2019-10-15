faa346ffc0f4a19925c5e4bd98bc8da7fd9dcd53
David Chan <dvxchan@gmail.com>
2019-10-14 23:44:18 -0400

    Updated port

--- src/environments/client-config.ts
+++ src/environments/client-config.ts
@@ -1,6 +1,6 @@
 const configurations = {
     port: 9000,
-    goliathDataServiceUrl: 'http://localhost:8101/',
+    goliathDataServiceUrl: 'http://localhost:8100/',
     armidilloMlServiceUrl: 'http://localhost:3000/'
 };
 
