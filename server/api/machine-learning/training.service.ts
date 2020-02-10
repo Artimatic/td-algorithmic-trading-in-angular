03557a4573e878c07cf43c8be6b085ba035e06a0
David Chan <dvxchan@gmail.com>
2020-02-09 19:46:27 -0500

    Added qqq and tlt to dataset

--- server/api/machine-learning/training.service.ts
+++ server/api/machine-learning/training.service.ts
@@ -10,17 +10,32 @@ class TrainingService {
   train(symbol, startDate, endDate) {
     const finalDataSet: TrainingData[] = [];
     let spyDataSet: TrainingData[];
+    let qqqDataSet: TrainingData[];
+    let tltDataSet: TrainingData[];
 
     return BacktestService.getTrainingData('SPY', endDate, startDate, false)
       .then(spyData => {
         spyDataSet = spyData;
+        return BacktestService.getTrainingData('QQQ', endDate, startDate, false);
+      })
+      .then(qqqData => {
+        qqqDataSet = qqqData;
+        return BacktestService.getTrainingData('TLT', endDate, startDate, false);
+      })
+      .then(tltData => {
+        tltDataSet = tltData;
         return BacktestService.getTrainingData(symbol, endDate, startDate, false);
       })
       .then((targetData: any[]) => {
         // if (targetData.length === spyDataSet.length) {
           spyDataSet.forEach((spyData, idx) => {
             const target = targetData[idx];
-            if (spyData.date === target.date) {
+            const qqq = qqqDataSet[idx];
+            const tlt = tltDataSet[idx];
+
+            if (spyData.date === target.date &&
+                qqq.date === target.date &&
+                tlt.date === target.date) {
               const dataSetObj = {
                 date: null,
                 input: null,
@@ -31,6 +46,8 @@ class TrainingService {
               dataSetObj.date = target.date;
               dataSetObj.input = [day]
                 .concat(spyData.input)
+                .concat(qqq.input)
+                .concat(tlt.input)
                 .concat(target.input);
 
               dataSetObj.output = target.output;
