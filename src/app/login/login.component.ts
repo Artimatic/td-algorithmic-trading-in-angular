4b5a79deee1d2349a55921d73783ca0da2b9f255
David Chan <dvxchan@gmail.com>
2020-08-23 20:58:41 -0400

    Removed mfa call

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   authenticated = false;
   hide = true;
-  mfa = false;
   model: any = {};
   loading = false;
   error = '';
@@ -75,44 +74,20 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.loading = true;
-    if (this.mfa) {
-      this.authenticationService.mfa(this.model.username, this.model.password, this.model.code)
-        .subscribe(result => {
-          if (result === true) {
-            this.mfa = true;
-            this.loading = false;
-            this.model = {};
-          } else {
-            // login failed
-            this.snackBar.open('Username or password is incorrect', 'Dismiss', {
-              duration: 2000,
-            });
-            this.loading = false;
-          }
-        },
-        error => {
-          this.snackBar.open('Username or password or Code is incorrect', 'Dismiss', {
-            duration: 2000,
-          });
+    this.authenticationService.login(this.model.username, this.model.password)
+      .subscribe(result => {
+        if (result === true) {
           this.loading = false;
-        });
-    } else {
-      this.authenticationService.login(this.model.username, this.model.password)
-        .subscribe(result => {
-          if (result === true) {
-            this.mfa = true;
-            this.loading = false;
-          } else {
-            this.loading = false;
-          }
-        },
-        error => {
-          this.snackBar.open('Username or password is incorrect', 'Dismiss', {
-            duration: 2000,
-          });
+        } else {
           this.loading = false;
+        }
+      },
+      error => {
+        this.snackBar.open('Username or password is incorrect', 'Dismiss', {
+          duration: 2000,
         });
-    }
+        this.loading = false;
+      });
   }
 
   saveTdaLogin() {
