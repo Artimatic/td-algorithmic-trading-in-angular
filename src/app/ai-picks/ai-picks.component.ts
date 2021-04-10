cad232ba189649c696957e494315b890ef17e78d
David Chan <dvxchan@gmail.com>
2021-04-10 18:05:48 -0400

    Updated ml list

--- src/app/ai-picks/ai-picks.component.ts
+++ src/app/ai-picks/ai-picks.component.ts
@@ -3,17 +3,24 @@ import { MachineLearningService } from '@shared/index';
 import { AiPicksService } from '@shared/services';
 import * as moment from 'moment';
 
+interface AiPicksPredictionData {
+  algorithm: number;
+  prediction: number;
+}
+
+interface AiPicksData {
+  label: string;
+  value: AiPicksPredictionData[];
+}
+
 @Component({
   selector: 'app-ai-picks',
   templateUrl: './ai-picks.component.html',
   styleUrls: ['./ai-picks.component.css']
 })
 export class AiPicksComponent implements OnInit, OnDestroy {
-  buys = [
-  ];
-
-  sells = [
-  ];
+  buys: AiPicksData[] = [];
+  sells: AiPicksData[] = [];
 
   buysLimit = 3;
   sellsLimit = 3;
@@ -34,7 +41,7 @@ export class AiPicksComponent implements OnInit, OnDestroy {
   }
 
   getPredictions(stock, isBuy) {
-    const SixtyDayPrediction = () => this.activate(stock, 60, 0.01, isBuy, () => {});
+    const SixtyDayPrediction = () => this.activate(stock, 60, 0.01, isBuy, () => { });
     const ThirtyDayPrediction = () => this.activate(stock, 30, 0.01, isBuy, SixtyDayPrediction);
     const FifteenDayPrediction = () => this.activate(stock, 15, 0.01, isBuy, ThirtyDayPrediction);
     const FiveDayPrediction = () => this.activate(stock, 5, 0.01, isBuy, FifteenDayPrediction);
@@ -50,19 +57,19 @@ export class AiPicksComponent implements OnInit, OnDestroy {
       limit)
       .subscribe((activation) => {
         if (!activation) {
-          this.trainAndActivate(symbol, range, limit, isBuy, cb);
+          setTimeout(() => {
+            this.trainAndActivate(symbol, range, limit, isBuy, cb);
+          }, 1000);
         } else {
-          const data = {
-            nextOutput: activation.nextOutput,
-            day: range,
-            symbol
-          };
+          const prediction = { algorithm: range, prediction: activation.nextOutput };
           if (isBuy) {
-            this.buys.push(this.createListObject(data));
+            this.addBuyPick(symbol, prediction);
           } else {
-            this.sells.push(this.createListObject(data));
+            this.addSellPick(symbol, prediction);
           }
-          cb();
+          setTimeout(() => {
+            cb();
+          }, 1000);
         }
         this.isLoading = false;
       }, error => {
@@ -92,8 +99,33 @@ export class AiPicksComponent implements OnInit, OnDestroy {
       });
   }
 
-  createListObject(data) {
-    return { label: data.symbol, value: { algorithm: data.day, prediction: data.nextOutput } };
+
+  addSellPick(symbol: string, predictionData: AiPicksPredictionData) {
+    const isSellPick = (element: AiPicksData) => element.label === symbol;
+
+    const index = this.sells.findIndex(isSellPick);
+    if (index >= 0) {
+      this.sells[index].value.push(predictionData);
+    } else {
+      const sellItem = this.createListObject(symbol, predictionData)
+      this.sells.push(sellItem);
+    }
+  }
+
+  addBuyPick(symbol: string, predictionData: AiPicksPredictionData) {
+    const isBuyPick = (element: AiPicksData) => element.label === symbol;
+
+    const index = this.sells.findIndex(isBuyPick);
+    if (index >= 0) {
+      this.buys[index].value.push(predictionData);
+    } else {
+      const item = this.createListObject(symbol, predictionData)
+      this.buys.push(item);
+    }
+  }
+
+  createListObject(symbol: string, predictionData: AiPicksPredictionData): AiPicksData {
+    return { label: symbol, value: [predictionData] };
   }
 
   ngOnDestroy() {
