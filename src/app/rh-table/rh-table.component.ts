0702f149130a6b5e3eb54668b3ce6d4b8b752f1c
dvxchan <dvxchan@gmail.com>
2021-10-22 03:02:17 -0400

    Close parentheses

--- src/app/rh-table/rh-table.component.ts
+++ src/app/rh-table/rh-table.component.ts
@@ -313,7 +313,7 @@ export class RhTableComponent implements OnInit, OnChanges, OnDestroy {
 
                       result.previousImpliedMovement = this.getPreviousImpliedMove(indicatorResults.signals[indicatorResults.signals.length - 2]);
                       
-                      result.kellyCriterion = (0.7-result.previousImpliedMovement)-((1-(0.7-result.previousImpliedMovement))/1 + result.previousImpliedMovement;
+                      result.kellyCriterion = (0.7-result.previousImpliedMovement)-((1-(0.7-result.previousImpliedMovement))/1 + result.previousImpliedMovement);
                       
                       const tableObj = {
                         recommendation: indicatorResults.recommendation,
