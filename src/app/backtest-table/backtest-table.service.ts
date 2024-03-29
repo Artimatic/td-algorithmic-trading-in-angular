8ee7798dd023b8fdcc5a2520256df1d2722ea957
dvxchan <dvxchan@gmail.com>
2024-03-28 21:28:32 -0400

    Updated to not save back tests with no signals

--- src/app/backtest-table/backtest-table.service.ts
+++ src/app/backtest-table/backtest-table.service.ts
@@ -31,53 +31,56 @@ export class BacktestTableService {
       indicatorResults.stock = symbol;
 
       const lastSignal = indicatorResults.signals[indicatorResults.signals.length - 1];
-
-      const optionsData = await this.optionsDataService.getImpliedMove(symbol).toPromise();
-      const instruments = await this.portfolioService.getInstrument(symbol).toPromise();
-      const callsCount = optionsData.strategy.secondaryLeg.totalVolume;
-      const putsCount = optionsData.strategy.primaryLeg.totalVolume;
-      const optionsVolume = Number(callsCount) + Number(putsCount);
-      let latestMlResult = { value: null };
-      try {
-        latestMlResult = await this.aiPicksService.trainAndActivate(symbol);
-      } catch(err) {
-        console.log('training error: ', new Date().toString(), latestMlResult);
-      }
-
-      const tableObj = {
-        recommendation: indicatorResults.recommendation,
-        stock: indicatorResults.stock,
-        returns: indicatorResults.returns,
-        total: indicatorResults.total,
-        invested: indicatorResults.invested,
-        profitableTrades: indicatorResults.profitableTrades,
-        totalTrades: indicatorResults.totalTrades,
-        lastVolume: indicatorResults.lastVolume || null,
-        totalReturns: indicatorResults.totalReturns || null,
-        lastPrice: indicatorResults.lastPrice || null,
-        ml: latestMlResult.value,
-        impliedMovement: optionsData.move,
-        optionsVolume: optionsVolume,
-        marketCap: instruments[symbol].fundamental.marketCap,
-        strongbuySignals: [],
-        buySignals: [],
-        strongsellSignals: [],
-        sellSignals: [],
-        high52: instruments[symbol].fundamental.high52
-      };
-
+      const buySignals = [];
+      const sellSignals = [];
       for (const indicator in lastSignal.recommendation) {
         if (lastSignal.recommendation.hasOwnProperty(indicator)) {
           if (lastSignal.recommendation[indicator] === 'Bullish') {
-            tableObj.buySignals.push(indicator);
+            buySignals.push(indicator);
           } else if (lastSignal.recommendation[indicator] === 'Bearish') {
-            tableObj.sellSignals.push(indicator);
+            sellSignals.push(indicator);
           }
         }
       }
 
-    this.addToResultStorage(tableObj);
-    return tableObj;
+      if (buySignals.length + sellSignals.length > 1) {
+        const optionsData = await this.optionsDataService.getImpliedMove(symbol).toPromise();
+        const instruments = await this.portfolioService.getInstrument(symbol).toPromise();
+        const callsCount = optionsData.strategy.secondaryLeg.totalVolume;
+        const putsCount = optionsData.strategy.primaryLeg.totalVolume;
+        const optionsVolume = Number(callsCount) + Number(putsCount);
+        let latestMlResult = { value: null };
+        try {
+          latestMlResult = await this.aiPicksService.trainAndActivate(symbol);
+        } catch (err) {
+          console.log('training error: ', new Date().toString(), latestMlResult);
+        }
+
+        const tableObj = {
+          recommendation: indicatorResults.recommendation,
+          stock: indicatorResults.stock,
+          returns: indicatorResults.returns,
+          total: indicatorResults.total,
+          invested: indicatorResults.invested,
+          profitableTrades: indicatorResults.profitableTrades,
+          totalTrades: indicatorResults.totalTrades,
+          lastVolume: indicatorResults.lastVolume || null,
+          totalReturns: indicatorResults.totalReturns || null,
+          lastPrice: indicatorResults.lastPrice || null,
+          ml: latestMlResult.value,
+          impliedMovement: optionsData.move,
+          optionsVolume: optionsVolume,
+          marketCap: instruments[symbol].fundamental.marketCap,
+          strongbuySignals: [],
+          buySignals: [],
+          strongsellSignals: [],
+          sellSignals: [],
+          high52: instruments[symbol].fundamental.high52
+        };
+
+        this.addToResultStorage(tableObj);
+        return tableObj;
+      }
     } catch (error) {
       console.log('Backtest table error', new Date().toString(), error);
     }
@@ -96,7 +99,7 @@ export class BacktestTableService {
       localStorage.setItem('backtest', JSON.stringify(newStorageObj));
     }
   }
-  
+
   addToBlackList(ticker: string) {
     const backtestBlacklist = JSON.parse(localStorage.getItem('blacklist'));
     if (backtestBlacklist) {
