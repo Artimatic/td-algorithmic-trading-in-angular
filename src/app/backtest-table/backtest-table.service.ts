c0802f91cea37cbe6ab98d521d515b37e2ebc0f7
dvxchan <dvxchan@gmail.com>
2024-03-29 21:44:19 -0400

    Fix backtest

--- src/app/backtest-table/backtest-table.service.ts
+++ src/app/backtest-table/backtest-table.service.ts
@@ -23,65 +23,64 @@ export class BacktestTableService {
       try {
         indicatorResults = await this.backtestService.getBacktestEvaluation(symbol, start, current, 'daily-indicators').toPromise();
         this.addToOrderHistoryStorage(symbol, indicatorResults.orderHistory);
+        indicatorResults.stock = symbol;
 
+        const lastSignal = indicatorResults.signals[indicatorResults.signals.length - 1];
+        const buySignals = [];
+        const sellSignals = [];
+        for (const indicator in lastSignal.recommendation) {
+          if (lastSignal.recommendation.hasOwnProperty(indicator)) {
+            if (lastSignal.recommendation[indicator] === 'Bullish') {
+              buySignals.push(indicator);
+            } else if (lastSignal.recommendation[indicator] === 'Bearish') {
+              sellSignals.push(indicator);
+            }
+          }
+        }
+  
+        if (buySignals.length + sellSignals.length > 1) {
+          const optionsData = await this.optionsDataService.getImpliedMove(symbol).toPromise();
+          const instruments = await this.portfolioService.getInstrument(symbol).toPromise();
+          const callsCount = optionsData.strategy.secondaryLeg.totalVolume;
+          const putsCount = optionsData.strategy.primaryLeg.totalVolume;
+          const optionsVolume = Number(callsCount) + Number(putsCount);
+          let latestMlResult = { value: null };
+          try {
+            latestMlResult = await this.aiPicksService.trainAndActivate(symbol);
+          } catch (err) {
+            console.log('training error: ', new Date().toString(), latestMlResult);
+          }
+  
+          const tableObj = {
+            recommendation: indicatorResults.recommendation,
+            stock: indicatorResults.stock,
+            returns: indicatorResults.returns,
+            total: indicatorResults.total,
+            invested: indicatorResults.invested,
+            profitableTrades: indicatorResults.profitableTrades,
+            totalTrades: indicatorResults.totalTrades,
+            lastVolume: indicatorResults.lastVolume || null,
+            totalReturns: indicatorResults.totalReturns || null,
+            lastPrice: indicatorResults.lastPrice || null,
+            ml: latestMlResult.value,
+            impliedMovement: optionsData.move,
+            optionsVolume: optionsVolume,
+            marketCap: instruments[symbol].fundamental.marketCap,
+            strongbuySignals: [],
+            buySignals: [],
+            strongsellSignals: [],
+            sellSignals: [],
+            high52: instruments[symbol].fundamental.high52
+          };
+  
+          this.addToResultStorage(tableObj);
+          return tableObj;
+        }
       } catch {
         indicatorResults = {
           stock: symbol
         };
         this.addToBlackList(symbol);
-      }
-      indicatorResults.stock = symbol;
-
-      const lastSignal = indicatorResults.signals[indicatorResults.signals.length - 1];
-      const buySignals = [];
-      const sellSignals = [];
-      for (const indicator in lastSignal.recommendation) {
-        if (lastSignal.recommendation.hasOwnProperty(indicator)) {
-          if (lastSignal.recommendation[indicator] === 'Bullish') {
-            buySignals.push(indicator);
-          } else if (lastSignal.recommendation[indicator] === 'Bearish') {
-            sellSignals.push(indicator);
-          }
-        }
-      }
-
-      if (buySignals.length + sellSignals.length > 1) {
-        const optionsData = await this.optionsDataService.getImpliedMove(symbol).toPromise();
-        const instruments = await this.portfolioService.getInstrument(symbol).toPromise();
-        const callsCount = optionsData.strategy.secondaryLeg.totalVolume;
-        const putsCount = optionsData.strategy.primaryLeg.totalVolume;
-        const optionsVolume = Number(callsCount) + Number(putsCount);
-        let latestMlResult = { value: null };
-        try {
-          latestMlResult = await this.aiPicksService.trainAndActivate(symbol);
-        } catch (err) {
-          console.log('training error: ', new Date().toString(), latestMlResult);
-        }
-
-        const tableObj = {
-          recommendation: indicatorResults.recommendation,
-          stock: indicatorResults.stock,
-          returns: indicatorResults.returns,
-          total: indicatorResults.total,
-          invested: indicatorResults.invested,
-          profitableTrades: indicatorResults.profitableTrades,
-          totalTrades: indicatorResults.totalTrades,
-          lastVolume: indicatorResults.lastVolume || null,
-          totalReturns: indicatorResults.totalReturns || null,
-          lastPrice: indicatorResults.lastPrice || null,
-          ml: latestMlResult.value,
-          impliedMovement: optionsData.move,
-          optionsVolume: optionsVolume,
-          marketCap: instruments[symbol].fundamental.marketCap,
-          strongbuySignals: [],
-          buySignals: [],
-          strongsellSignals: [],
-          sellSignals: [],
-          high52: instruments[symbol].fundamental.high52
-        };
-
-        this.addToResultStorage(tableObj);
-        return tableObj;
       }
     } catch (error) {
       console.log('Backtest table error', new Date().toString(), error);
